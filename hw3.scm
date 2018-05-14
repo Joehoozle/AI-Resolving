2bbf94c22fa5b0b92f97f603fb685833e681ba24
Joehoozle <dantedg@comcast.net>
2018-05-13 23:24:29 -0700

    working with prints

--- hw3.scm
+++ hw3.scm
@@ -41,16 +41,23 @@
 
 ; main funciton for performing resolve
 (define (res-helper list1 list2 sol chosen)
+    
+    (display list1)
+    (display list2)
+    (display sol)
+    (display chosen)
+    (newline)
+    (newline)
     (cond
-        
+
         ; if there are no chosen pairs, the stamements cannot be resolved
         ((and (null? list1) (null? chosen)) #f)     
         
         ; if there are more than one chosen pairs, the statements cannot be resolved
         ((and (null? list1) (multiple-elements chosen)) #f)
 
-        ; if there is a contradiction
-        ((and (null? list1) (null? sol)) "CONTRADICTION")
+        ; if there is a contradiction, meaning 
+        ((and (and (null? list1) (null? sol)) (null? (res-delete list2 (car chosen)))) 'CONTRADICTION)
 
         ; if one chosen pair has been found and the process is over
         ((null? list1) (append sol (res-delete list2 (car chosen))))
@@ -58,9 +65,10 @@
         ; normal case for checking for pairs
         (#t (let ((value (res-search (car list1) (negate (car list1)) list2))) 
             (cond 
+                ; if you found the negative of current, add it to chosen to be removed from the list 
                 ((equal? value "negative") (res-helper (cdr list1) list2 sol (append (list (negate (car list1))) chosen)))
-                ((equal? value "same") 'cool)
-                ;((equal? value "positive") (res-helper (cdr list1) list2 (append sol (car list1)) chosen (append (car list1) duplicates)))
+                ; ignore the first of two duplicates
+                ((equal? value "same") (res-helper (cdr list1) list2 sol chosen))
                 (#t (res-helper (cdr list1) list2 (append sol value) chosen))
             )
         )) 
@@ -69,3 +77,110 @@
 
 ; wrapper function for resolve
 (define (res list1 list2) (res-helper list1 list2 '() '()))
+
+
+;--------------------------------------------------------------------------------------------------------------------------------------------------
+
+; INTERMEDIATE ASSIGNMENT
+
+; define the knowledge base
+(define kb '())
+
+; adds a statement to the knoweledge base
+(define (tell stmt)
+    (display "telling")
+    (newline)
+    (begin
+        (set! kb (append kb (list stmt)))
+        'OK
+    )
+)
+
+; initial run through the knowledge base to see if the stament is there
+(define (knowledge-check-helper stmt kb-cur)
+    (display "knowledge check helper")
+    (newline)
+    (cond
+        ((null? kb-cur) #f)
+        ((equal? stmt (car kb-cur)) #t)
+        (#t (knowledge-check-helper stmt (cdr kb-cur)))    
+    )
+)
+
+
+(define (knowledge-check stmt) (knowledge-check-helper stmt kb))
+
+
+; actually resolving the statement
+(define (resolve-stmt-helper stmt kb-cur-stmt)
+    (display "resolving stmt helper")
+    (newline)
+    (display "...............")
+    (newline)
+    (display stmt)
+    (newline)
+    (display kb-cur-stmt)
+    (newline)
+    (display kb)
+    (newline)
+    (newline)
+    (let ((new (res stmt kb-cur-stmt)))
+        (cond
+            ((and (not (equal? new #f)) (not (knowledge-check new))) 
+                (begin 
+                    (tell new) 
+                    new
+                )              
+            )
+        )
+    ) 
+)
+
+; resolve every statment against current one
+(define (resolve-stmt ask-stmt stmt kb-cur)
+    (display "resolving stmt")
+    (newline)
+    (cond
+        ((null? kb-cur) #f)
+        ((equal? ask-stmt (resolve-stmt-helper stmt (car kb-cur))) #t)
+        (#t (resolve-stmt ask-stmt stmt (cdr kb-cur)))    
+    )
+)
+
+; because we kb is changing as we are resolving things, we want an updated knowledege base every time we
+; start exhaustively resolving for the next element
+(define (kb-cur-update stmt kb-cur)
+    (display "updating current")
+    (newline)
+    (cond 
+        ((equal? stmt (car kb-cur)) (cdr kb-cur))
+        (#t (kb-cur-update stmt (cdr kb-cur)))
+    )
+)
+
+; attempt to resolve the first element that we have of kb with all other elements and add those new resolutions to kb
+(define (resolve-stmts-helper ask-stmt kb-cur)
+    (display ask-stmt)
+    (newline)
+    (cond 
+        ((null? kb-cur) #f)
+        (#t (begin
+                (cond 
+                    ((resolve-stmt ask-stmt (car kb-cur) (cdr kb-cur)) #t)
+                    (#t (resolve-stmts-helper ask-stmt (kb-cur-update (car kb-cur) kb)))
+                )
+            )
+        )
+    )
+)
+
+(define (resolve-stmts ask-stmt) (resolve-stmts-helper ask-stmt kb))
+
+; return true if you can resolve to the statement, or unknown
+(define (ask ask-stmt) 
+    (cond 
+        ((knowledge-check ask-stmt) #t)
+        ((resolve-stmts ask-stmt) #t)
+        (#t 'UNKNOWN)
+    )    
+)
